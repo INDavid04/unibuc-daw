eefe7703c247a16dc758292b7a3715aacf93c5ee
INDavid04 <davidirimia100@gmail.com>
2025-12-19 15:45:49 +0200

    fix: curs-contact links

--- curs-contact/verify-recaptcha.php
+++ curs-contact/verify-recaptcha.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IND | DAW Contact Captcha</title>
+    <title>IND | DAW Formular de contact trimis</title>
     <link rel="stylesheet" href="../assets/css/styles.css">
 	<!-- Captcha -->
 	<script src="https://www.google.com/recaptcha/api.js" async defer></script>
@@ -44,35 +44,95 @@
     </header>
 
     <main>
-        <div id="central">
-			<div class="content">
-				<h1>Formular contact</h1>
-				<p>Te rugam sa ne transmiti informatiile de mai jos:</p>
-                <p>Pagina realizata in cadrul cursului cu numarul 11</p>
-				<div id="message">
-					<form action="verify-recaptcha.php" method="post">
-						<div class="label">Nume:</div>
-							<div class="field">
-								<input type="text" id="name" name="name" class="required" aria-required="true" required>
-							</div>
-						<div class="label">Email:</div>
-						<div class="field">			
-							<input type="email" id="email" name="email" class="required email" aria-required="true" required>
-						</div>
-						<div class="label">Telefon:</div>
-						<div class="field">			
-							<input type="tel" id="phone" name="phone" class="required phone" aria-required="true" required>
-						</div>
-						<div class="label">Mesaj:</div>
-						<div class="field">			
-							<textarea id="comment-content" name="content"></textarea>			
-						</div>
-						<div class="g-recaptcha" data-sitekey="6LexKSYsAAAAAC-vA7ryGqTZJ_aQKWSVFE4qyBcG"></div>
-						<input class="btn btn-info" type="submit" name="submit" value="SUBMIT" >
-					</form>
-				</div>		
-			</div>
-		</div>
+        <h1>Formularul de contact a fost trimis</h1>
+		<?php 
+
+			/// reCAPTCHA1: https://www.google.com/recaptcha/admin/site/740695923/setup
+			/// reCAPTCHA2: https://www.google.com/recaptcha/admin/site/740698545/setup
+
+			$returnMsg = ''; 
+			
+			if(isset($_POST['submit'])){ 
+				
+				// Form fields validation check
+				if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['phone'])){ 
+					
+					// reCAPTCHA checkbox validation
+					if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])){ 
+						// Google reCAPTCHA API secret key 
+						$secret_key = '6LexKSYsAAAAAKI7sYq92qBMzry5pLS1-JZaqVFh'; 
+						
+						// reCAPTCHA response verification
+						$verify_captcha = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret_key.'&response='.$_POST['g-recaptcha-response']); 
+						
+						// Decode reCAPTCHA response 
+						$verify_response = json_decode($verify_captcha); 
+						
+						// Check if reCAPTCHA response returns success 
+						if($verify_response->success){ 
+							
+							$name = $_POST['name']; 
+							$email = $_POST['email']; 
+							$phone = $_POST['phone'];
+							$message = $_POST['content'];
+						
+							// Gmail
+							require_once('../curs-mail/class.phpmailer.php');
+							require_once('../curs-mail/mail_config.php');
+							
+							$mailBody = "User Name: " . $name . "\n";
+							$mailBody .= "User Email: " . $email . "\n";
+							$mailBody .= "Phone: " . $phone . "\n";
+							$mailBody .= "Message: " . $message . "\n";
+							
+							$mail = new PHPMailer(true); 
+
+							$mail->IsSMTP();
+
+							try {
+							
+							$mail->SMTPDebug  = 3;                     
+							$mail->SMTPAuth   = true; 
+
+							$toEmail='contact11@dirimia.daw.ssmr.ro';
+							$nume='DAW Project';
+
+							$mail->SMTPSecure = "tls";                 
+							$mail->Host       = "mail.dirimia.daw.ssmr.ro";      
+							$mail->Port       = 587;                   
+							$mail->Username   = $username;  			// GMAIL username
+							$mail->Password   = $password;            // GMAIL password
+							$mail->AddReplyTo($email, $name);
+							$mail->AddAddress($toEmail, $nume);
+							$mail->addCustomHeader("BCC: ".$email);
+							
+							$mail->SetFrom($username, 'Formular Contact');
+							$mail->From = $username;
+							$mail->Subject = 'Formular contact';
+							$mail->AltBody = 'To view this post you need a compatible HTML viewer!'; 
+							$mail->MsgHTML($mailBody);
+							
+							$mail->Send();
+							
+							$returnMsg = 'Your message has been submitted successfully.'; 
+							
+							} catch (phpmailerException $e) {
+								echo $e->errorMessage(); //error from PHPMailer
+							}
+							
+						} 
+					} else { 
+						
+						$returnMsg = 'Please check the CAPTCHA box.'; 
+					} 
+				} else { 
+					$returnMsg = 'Please fill all the required fields.'; 
+				} 
+			} 
+
+			echo $returnMsg;
+
+		?>
     </main>
 
     <footer>
@@ -91,3 +151,4 @@
     </footer>
 </body>
 </html>
+
